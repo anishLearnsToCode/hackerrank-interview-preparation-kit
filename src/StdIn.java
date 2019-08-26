a236121a5b8172d1d6f888186b13063bb79cd4cd
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-08-26 23:18:01 +0200

    solves ney year chaos

---
+++ src/StdIn.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class StdIn {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    static int[] getIntArray() {
+        int length = getInt();
+        int[] array = new int[length];
+        for (int index = 0 ; index < array.length ; index++) {
+            array[index] = getInt();
+        }
+        return array;
+    }
+
+    static int getInt() {
+        return SCANNER.nextInt();
+    }
+}
