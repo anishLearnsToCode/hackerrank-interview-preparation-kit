a92215fff4601e12f3672cc95c9ed5082d2b660b
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-06-01 20:14:01 +0530

    solves jumping on theclouds

---
+++ src/warmup/JumpingOnTheClouds.java
@@ -0,0 +1,48 @@
+package warmup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class JumpingOnTheClouds {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int length = scanner.nextInt();
+        int[] array = getArray(length);
+        System.out.println(minimumJumps(array));
+    }
+
+    private static int[] getArray(int length) {
+        int[] array = new int[length];
+        for (int index = 0 ; index < array.length ; index++) {
+            array[index] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    private static int minimumJumps(int[] array) {
+        return minimumJumps(array, 0, new HashMap<>());
+    }
+
+    private static int minimumJumps(int[] array, int currentPosition, Map<Integer, Integer> memory) {
+        if (currentPosition >= array.length || array[currentPosition] == 1) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (currentPosition == array.length - 1) {
+            return 0;
+        }
+
+        if (memory.containsKey(currentPosition)) {
+            return memory.get(currentPosition);
+        }
+
+        int answer = 1 + Math.min(
+                minimumJumps(array, currentPosition + 1, memory),
+                minimumJumps(array, currentPosition + 2, memory)
+        );
+        memory.put(currentPosition, answer);
+        return answer;
+    }
+}
